7477a1d524864f585ac429f35d97c29e30b74eeb
Mitchell Lee <mitchell.lee@hsw.com.au>
2021-05-11 20:25:37 +1000

    Fixed documentation & added implemenation

---
+++ documentation/an-example-doc.md
@@ -0,0 +1,28 @@
+---
+title: 'An Example Doc'
+date: '2020-12-19'
+---
+
+Text, text, text, <a href="https://hsw.com.au">link</a>
+
+## TITLE
+# SUB-TITLE
+
+**BOLD**
+- a
+- 2
+- z
+
+Paragraph, etc.....
+
+
+Description of code (rust in this case)
+```rust
+pub fn add(num1: usize, num2: usize) -> usize {
+    println!("Sum = {}", num1 + num2);
+    num1 + num2
+}
+```
+<br></br>
+
+End of documentation.
--- pages/documentation/index.tsx
+++ pages/documentation/index.tsx
@@ -1,7 +1,10 @@
 import { FC } from 'react';
 import { GetStaticProps } from 'next'
-import Documentation from '@components/documentation';
+import { useMediaQuery } from "react-responsive";
+import Layout from '@components/layout';
+import DocumentationList from '@components/documentation';
 import { getSortedDocumentationData, DocumentMetaData } from '@lib/documents';
+import utilStyles from '@styles/utils.module.css';
 
 export const getStaticProps: GetStaticProps = async () => {
   const allDocumentation = await getSortedDocumentationData()
@@ -12,8 +15,28 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-const Document: FC<{ allDocumentation: DocumentMetaData[] }> = ({ allDocumentation }) => {
-  return (<Documentation documents={allDocumentation} />)
+const meta = {
+  title: "HSW Boomi API Documentation",
+  description: "A place collection of documentation on HSW's Boomi APIs."
+};
+
+const Documentation: FC<{ allDocumentation: DocumentMetaData[] }> = ({ allDocumentation }) => {
+  const shouldBe2Cols = useMediaQuery({ minWidth: '40rem' });
+  return (
+    <Layout title="Documentation | HSW Boomi API" meta={meta}>
+        {shouldBe2Cols ? (
+          <div className={utilStyles.flexGrid}>
+            <DocumentationList documents={allDocumentation} style={{ maxWidth: "20rem" , padding: '0.5rem' }} />
+            <section className={`${utilStyles.headingMd} ${utilStyles.flexColumn} ${utilStyles.sticky}`} style={{ maxWidth: "20rem" , padding: '0.5rem' }}>
+              <h2 className={utilStyles.headingLg}>Documentation Intro</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eaque quae architecto, praesentium, sapiente ratione quia dolorum labore nobis nostrum porro vero aliquam vitae totam iste excepturi dolorem fugiat doloribus.</p>
+            </section>
+          </div>
+        ) : (
+          <DocumentationList documents={allDocumentation} style={{ maxWidth: "20rem", margin: "auto" }} />
+        )} 
+      </Layout>
+    )
 }
 
-export default Document;
+export default Documentation;
